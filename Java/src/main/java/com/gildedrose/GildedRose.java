e90a0a4f9cddadb711fec1fc2b0ed2573a818876
jeremy.ting <jeremy.ting@104.com.tw>
2020-07-20 18:01:19 +0800

    remove unnecessary code

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -17,28 +17,26 @@ class GildedRose {
     	 if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
             return;
          }
+    	 
+    	 item.sellIn = item.sellIn - 1;
+    	 
 	     if (item.name.equals("Aged Brie")) {
 	    	 increaseQuality(item);
 	     }else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {	 
 	    	 increaseQuality(item);
-	   		 
-	    	 if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-	             if (item.sellIn < 11) {
-	            	 increaseQuality(item);
-	             }
-	
-	             if (item.sellIn < 6) {
-	            	 increaseQuality(item);
-	             }
-	         }
+	    	 
+             if (item.sellIn < 11) {
+            	 increaseQuality(item);
+             }
+
+             if (item.sellIn < 6) {
+            	 increaseQuality(item);
+             }
 	   	 }else {
 	   		decreaseQuality(item);
 	   	 }
-   	
-	   item.sellIn = item.sellIn - 1;
-       
 
-       if (item.sellIn < 0) {
+         if (item.sellIn < 0) {
 	       if (item.name.equals("Aged Brie")) {
 	       		increaseQuality(item);
 	       	}else {
